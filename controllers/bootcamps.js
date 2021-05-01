d64d6ee99d8e26164a9f39fa2853f52689aa068a
Sajid16 <sajid.mahboob16@gmail.com>
2021-05-02 00:49:34 +0600

    started working on developer bootcamp API project

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,34 +1,111 @@
+const { findByIdAndUpdate } = require('../models/Bootcamp');
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc  Get all bootcamps
 // @route GET /api/v1/bootcamps
 // @access public
-exports.getBootcamps = (req, res, next) => {
-  res.send({ message: 'get all bootcamp routes' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.find();
+    res.status(200).json({
+      success: true,
+      count: bootcamp.length,
+      data: bootcamp,
+      message: 'get all bootcamp data',
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc  Get single bootcamp
 // @route GET /api/v1/bootcamps/:id
 // @access public
-exports.getBootcampById = (req, res, next) => {
-  res.send({ message: `bootcamp routes ${req.params.id}` });
+exports.getBootcampById = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+      message: `bootcamp data for id ${req.params.id}`,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc  create bootcamps
 // @route POST /api/v1/bootcamps
 // @access private
-exports.createBootcamp = (req, res, next) => {
-  res.send({ message: 'create bootcamp routes' });
+exports.createBootcamp = async (req, res, next) => {
+  // console.log(req.body);
+
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      message: 'new bootcamp created',
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error,
+    });
+  }
 };
 
 // @desc  update bootcamp
 // @route PUT /api/v1/bootcamps/:id
 // @access private
-exports.updateBootcamp = (req, res, next) => {
-  res.send({ message: `update bootcamp routes ${req.params.id}` });
+exports.updateBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, //new data after update show korbe
+      runValidators: true, // mongoose er validators check korbe kina
+    });
+
+    if (!bootcamp) {
+      res.status(400).json({
+        success: false,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error,
+    });
+  }
 };
 
 // @desc  delete bootcamps
 // @route DELETE /api/v1/bootcamps/:id
 // @access private
-exports.deleteBootcamp = (req, res, next) => {
-  res.send({ message: `delete bootcamp routes ${req.params.id}` });
+exports.deleteBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    // console.log(bootcamp);
+    if (!bootcamp) {
+      res.status(400).json({
+        success: false,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: `Record succesfully deleted for id ${req.params.id}`,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
--- server.js
+++ server.js
@@ -1,21 +1,55 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 // load env variables
-
 dotenv.config({ path: './config/config.env' });
 
+// connecting to database
+connectDB();
+
 const app = express();
+
+// body parser
+app.use(express.json());
+
+// creating middleware
+const logger = (req, res, next) => {
+  req.hello = 'Hello Sajid!';
+  console.log('middleware is woking');
+  next();
+};
+
+// below line indicates that the created middleware is ready to use
+// below line er jonne all controller function a middleware access pabo
+app.use(logger);
+
+// end of middleware
+
+// initilizing routes
+// app testing api
+app.get('/', (req, res) => {
+  return res.send({
+    message: `Devcamper API project is working on port ${PORT} successfully`,
+  });
+});
 
 // route files
 const bootcamps = require('./routes/bootcamps.js');
 
-// initilizing routess
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// it is a global handler for unhandled promise rejection, helps to skip try catch block
+// handling unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // closing server and exit process
+  server.close(() => process.exit(1));
+});
