6facf4280f7c62434cb24e391ddabaf80df39446
Sajid16 <sajid.mahboob16@gmail.com>
2021-07-03 01:55:46 +0600

    commit on 3rd july

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {
-    userNewUrlParser: true,
+    useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
