0aebd91af9ab1ec08237d1eaf740d1a843f051de
Sajid16 <sajid.mahboob16@gmail.com>
2021-09-05 00:30:53 +0600

    slug and geocode added with mongoose middleware

---
+++ middleware/error.js
@@ -0,0 +1,10 @@
+const errorHandler = (err, req, res, next) => {
+    console.log(err);
+
+    res.status(err.statusCode || 500).json({
+        success: false,
+        error: err.message || 'Server Error'
+    });
+}
+
+module.exports = errorHandler;
